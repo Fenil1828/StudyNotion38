a396797279896e1e0dd37bf7550f5f97cf27ae75
Fenil1828 <jasanifenil18@gmail.com>
2025-07-18 20:02:26 +0530

    Update apis.js

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://studynotion38-6.onrender.com/api/v1"
+const BASE_URL = "https://study-notion38-f.vercel.app/api/v1"
 
 export const categories = {
     CATEGORIES_API : BASE_URL + "/course/showAllCategories",
