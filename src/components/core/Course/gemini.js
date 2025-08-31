ab587b46d184bb457333dfd0ec671d1637fc6d36
Fenil1828 <jasanifenil18@gmail.com>
2025-08-31 10:18:32 +0530

    Update gemini.js

--- src/components/core/Course/gemini.js
+++ src/components/core/Course/gemini.js
@@ -1,12 +1,12 @@
 // gemini.js
-REACT_APP_GEMINI_API_KEY = AIzaSyCvS2kPyLYyKQYuhJalrpFZsJmJyldqcGo;
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI("AIzaSyCvS2kPyLYyKQYuhJalrpFZsJmJyldqcGo");
 
 export async function generateGeminiResponse(prompt) {
   const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
   const result = await model.generateContent(prompt);
   return result.response.text();
 }
+
 
