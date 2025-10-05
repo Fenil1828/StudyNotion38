b1a9556fde32b0a0337f56376046695096f033b4
Fenil1828 <jasanifenil18@gmail.com>
2025-10-05 10:51:05 +0530

    Update ContactDetails.jsx

--- src/components/core/ContactUsPage/ContactDetails.jsx
+++ src/components/core/ContactUsPage/ContactDetails.jsx
@@ -9,7 +9,7 @@ const contactDetails = [
     {
         icon: "HiChatBubbleLeftRight",
         heading: "Chat on us",
-        description: "Our friendly team is here to help",
+        description: "Our friendly team is here to help - Fenil Jasani",
         details: "info@studynotion.com",
     },
     {
@@ -17,13 +17,13 @@ const contactDetails = [
     heading: "Visit us",
     description: "Come and say hello at our office HQ.",
     details:
-      "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+      "VDev Infra in katargam, Surat",
   },
   {
     icon: "IoCall",
     heading: "Call us",
     description: "Mon - Fri From 8am to 5pm",
-    details: "+123 456 7869",
+    details: "99788 17816",
   },
 
 
